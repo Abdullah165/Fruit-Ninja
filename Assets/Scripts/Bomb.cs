31afb41d98d6b22cb8d0a10f47b9501986aaf11e
Abdullah Abdelaty <63372032+Abdullah165@users.noreply.github.com>
2023-03-29 13:20:40 +0200

    Update Bomb.cs

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,8 +5,26 @@ public class Bomb : MonoBehaviour, IDamageable
 {    
     public static event EventHandler OnGameOver;
 
+    [SerializeField] GameObject explosionPrefab;
+    [SerializeField] AudioSource audioSource;
+
     public void Damage()
     {
+        //Hide the bomb gameObject
+        //play explosion effect
+        //play the explosion sound effect
+
+        gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        audioSource.Play();
+
+        StartCoroutine(LoadGameOverScene());
+    }
+
+    IEnumerator LoadGameOverScene()
+    {
+        yield return new WaitForSeconds(0.7f);
+
         OnGameOver?.Invoke(this, EventArgs.Empty);
     }
 }
